21a7f1f11765df75ed78eee734ea303f4ae08ea0
Jan Behrens <zykure@web.de>
2014-10-27 23:59:10 +0100

    Fix AUR build, add GTK2 and GTK3 versions

--- makeaur.sh
+++ makeaur.sh
@@ -5,7 +5,6 @@ FLAG=$1
 OLDPWD=`pwd`
 
 PACKAGENAME=deadbeef-fb
-PACKAGEREL=1
 
 DATE=`date +%Y%m%d`
 BINTARGET=${OLDPWD}/../${PACKAGENAME}${FLAG}_${DATE}.tar.gz
@@ -14,13 +13,29 @@ SRCTARGET=${OLDPWD}/../${PACKAGENAME}${FLAG}_${DATE}_src.tar.gz
 MD5SUM=$(md5sum ${SRCTARGET} | cut -c -32)
 SHA1SUM=$(sha1sum ${SRCTARGET} | cut -c -40)
 
-cat PKGBUILD.in \
-    | sed s/@PACKAGENAME@/${PACKAGENAME}/g \
-    | sed s/@PACKAGEREL@/${PACKAGEREL}/g \
-    | sed s/@DATE@/${DATE}/g \
-    | sed s/@MD5SUM@/${MD5SUM}/g \
-    | sed s/@SHA1SUM@/${SHA1SUM}/g \
-    > PKGBUILD
-mkaurball
-mv -v ${PACKAGENAME}-${DATE}-${PACKAGEREL}.src.tar.gz ${OLDPWD}/../
-rm -f PKGBUILD
+function make_package
+{
+    AURPACKAGENAME=$1
+    AURPACKAGEFLAG=$2
+    AURPACKAGEREL=$3
+    AURPACKAGEDEPS=$4
+
+    echo "Building AUR package ${AURPACKAGENAME}${AURPACKAGEFLAG}-${DATE}-${AURPACKAGEREL} ..."
+
+    cat PKGBUILD.in \
+        | sed s/@PACKAGENAME@/${AURPACKAGENAME}/g \
+        | sed s/@PACKAGEFLAG@/${AURPACKAGEFLAG}/g \
+        | sed s/@PACKAGEREL@/${AURPACKAGEREL}/g \
+        | sed s/@PACKAGEVER@/${DATE}/g \
+        | sed s/@PACKAGEDEPS@/${AURPACKAGEDEPS}/g \
+        | sed s/@SOURCENAME@/${PACKAGENAME}${FLAG}_${DATE}/g \
+        | sed s/@MD5SUM@/${MD5SUM}/g \
+        | sed s/@SHA1SUM@/${SHA1SUM}/g \
+        > PKGBUILD
+    mkaurball
+    mv -v ${AURPACKAGENAME}${AURPACKAGEFLAG}-${DATE}-${AURPACKAGEREL}.src.tar.gz ${OLDPWD}/../
+    rm -f PKGBUILD
+}
+
+make_package "deadbeef-plugin-fb" ""      1 gtk2
+make_package "deadbeef-plugin-fb" "-gtk3" 1 gtk3
