6d29d755247a0de49fe0b0fe76bfd936af21a183
Jan Behrens <zykure@web.de>
2015-09-22 21:40:56 +0200

    Add another version check

--- filebrowser.c
+++ filebrowser.c
@@ -2207,6 +2207,7 @@ get_icon_for_uri (gchar *uri)
 
     if (! g_file_test (uri, G_FILE_TEST_IS_DIR))
     {
+#if GIO_CHECK_VERSION(2, 34, 0)
         gchar *content_type;
         gchar *icon_name;
         GtkIconTheme *icon_theme;
@@ -2219,7 +2220,7 @@ get_icon_for_uri (gchar *uri)
 
         g_free (content_type);
         g_free (icon_name);
-
+#endif
         // Fallback to default icon
         if (! icon)
             icon = utils_pixbuf_from_stock ("gtk-file", CONFIG_ICON_SIZE);
